17aa0f48210853c1c2583c5d0bf55ae2aabcaf67
Roland <roland.jorz@gmail.com>
2026-08-16 00:09:08 +0300

    Bump release version to 1.1.1

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 /** Canonical application release metadata shared by the page and service worker. */
 (function (global) {
-    const version = '1.1.0';
+    const version = '1.1.1';
 
     global.BreathingApp = Object.freeze({
         version,
